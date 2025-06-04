56cc23b5fff797da33abdffb7239c34b6af4c50e
sidantha0377 <150885218+sidantha0377@users.noreply.github.com>
2025-06-04 23:09:35 +0530

    ss

--- src/components/Services/api.js
+++ src/components/Services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Link } from "react-router-dom";
-const API_URL = "https://ec2-3-88-237-151.compute-1.amazonaws.com:9090/api/v1";
+const API_URL =
+  "http://smartlocker-backend-bkf3bydrfbfjf4g8.southindia-01.azurewebsites.net/api/v1"; //"https://ec2-3-88-237-151.compute-1.amazonaws.com:9090/api/v1";
 //"http://smartlocker-backend-bkf3bydrfbfjf4g8.southindia-01.azurewebsites.net/api/v1";
 
 const api = axios.create({
